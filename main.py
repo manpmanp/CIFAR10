dc8d5311b3bfca2cd8bac7a888de7b4498a9c48c
manpmanp <manp.social@gmail.com>
2023-04-25 09:19:45 +0200

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,8 @@ import os
 import argparse
 import numpy as np
 from utils import progress_bar, get_lr, recycle
-from efficientnet_pytorch import EfficientNet
+#from efficientnet_pytorch import EfficientNet
+import nvidia_efficientnet_b4
 
 parser = argparse.ArgumentParser(description='PyTorch CIFAR10 Training')
 parser.add_argument('--lr', default=0.01, type=float, help='learning rate')
